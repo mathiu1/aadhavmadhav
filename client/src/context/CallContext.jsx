da761e4dadd251bfd979c1382376129b12ed87f8
mathivan07 <mathivan07@gmail.com>
2026-01-05 18:30:13 +0530

    Update CallContext.jsx

--- client/src/context/CallContext.jsx
+++ client/src/context/CallContext.jsx
@@ -261,7 +261,23 @@ export const CallContextProvider = ({ children }) => {
                     config: {
                         iceServers: [
                             { urls: 'stun:stun.l.google.com:19302' },
-                            { urls: 'stun:global.stun.twilio.com:3478' }
+                            { urls: 'stun:global.stun.twilio.com:3478' },
+                            // FREE PUBLIC TURN (OpenRelay) - Guarantees connectivity through Symmetric NAT
+                            {
+                                urls: 'turn:openrelay.metered.ca:80',
+                                username: 'openrelayproject',
+                                credential: 'openrelayproject'
+                            },
+                            {
+                                urls: 'turn:openrelay.metered.ca:443',
+                                username: 'openrelayproject',
+                                credential: 'openrelayproject'
+                            },
+                            {
+                                urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+                                username: 'openrelayproject',
+                                credential: 'openrelayproject'
+                            }
                         ],
                     }
                 });
@@ -357,7 +373,23 @@ export const CallContextProvider = ({ children }) => {
                     config: {
                         iceServers: [
                             { urls: 'stun:stun.l.google.com:19302' },
-                            { urls: 'stun:global.stun.twilio.com:3478' }
+                            { urls: 'stun:global.stun.twilio.com:3478' },
+                            // FREE PUBLIC TURN (OpenRelay) - Guarantees connectivity through Symmetric NAT
+                            {
+                                urls: 'turn:openrelay.metered.ca:80',
+                                username: 'openrelayproject',
+                                credential: 'openrelayproject'
+                            },
+                            {
+                                urls: 'turn:openrelay.metered.ca:443',
+                                username: 'openrelayproject',
+                                credential: 'openrelayproject'
+                            },
+                            {
+                                urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+                                username: 'openrelayproject',
+                                credential: 'openrelayproject'
+                            }
                         ],
                     }
                 });

3de3f29e8abb1afb500b0003ec322ad23fb85f80
mathivan07 <mathivan07@gmail.com>
2026-01-09 17:33:14 +0530

    disable the ping api

--- server/ping.js
+++ server/ping.js
@@ -3,11 +3,11 @@ const URL = "https://aadhavmadhav.onrender.com/api/ping";
 console.log(`Keep-alive ping configured for: ${URL}`);
 
 // Using native Node.js fetch (Node 18+)
-setInterval(async () => {
-  try {
-    const res = await fetch(URL);
-    // console.log("Ping success:", res.status);
-  } catch (err) {
-    console.error("Ping failed:", err.message);
-  }
-}, 5 * 60 * 1000);
+// setInterval(async () => {
+//   try {
+//     const res = await fetch(URL);
+//     // console.log("Ping success:", res.status);
+//   } catch (err) {
+//     console.error("Ping failed:", err.message);
+//   }
+// }, 5 * 60 * 1000);
